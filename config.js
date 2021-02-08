2fd5cbe435354dc89eec11c35a9f8099b27e1c39
Mike <mikegarite@gmail.com>
2021-02-07 21:43:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
     host: env.DB_HOST || 'getreal.cxgbtkv0eyak.us-west-2.rds.amazonaws.com',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'postgress',
+    user: env.DB_USER || 'postgres',
     password: env.DB_PASSWORD || 'gomrgvgo!!77',
     database: env.DB_NAME || 'getreal',
   },
